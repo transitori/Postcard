258b78ee62bae57b44d70c5f214e26179c5c2f04
Monika <monika@enthusio.com>
2014-11-13 16:46:36 +0100

    first rev of project

--- postcard-project/ViewController.swift
+++ postcard-project/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
     
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
     
+    @IBOutlet weak var enterMessageTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,6 +26,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        println("hello")
+        messageLabel.text = enterMessageTextField.text
+        messageLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+    }
 
 }
 
